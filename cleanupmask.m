c3176bdc50db5ac092c3493052cdc371d1fde0b9
XiaotongLu <31257663+XiaotongLu@users.noreply.github.com>
2017-10-19 12:01:42 -0500

    Update cleanupmask.m

--- cleanupmask.m
+++ cleanupmask.m
@@ -1,10 +1,5 @@
 function img_dilate=cleanupmask(image)
-%I=imread('D:\matlab_P3_3.tif');
-%J=uint16(I);
- %img_double=im2double(J);
  img_dilate=imdilate(image,strel('disk',15));
- %img_norm=img_double./img_dilate;img_double
- %img_norm_dilate=imdilate(img_norm,strel('disk',10));
  imwrite(img_dilate,'D:\matlab_P3_4.tif','tif');
  imshow(img_dilate,[])
 end
