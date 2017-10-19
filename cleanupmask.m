7f4c12f2201621e776fcbe63eaa7961dcf006260
XiaotongLu <31257663+XiaotongLu@users.noreply.github.com>
2017-10-19 15:53:03 -0500

    Update cleanupmask.m

--- cleanupmask.m
+++ cleanupmask.m
@@ -1,5 +1,25 @@
 function img_dilate=cleanupmask(image)
- img_dilate=imdilate(image,strel('disk',15));
- imwrite(img_dilate,'D:\matlab_P3_4.tif','tif');
- imshow(img_dilate,[])
+img_bw=imdilate(image,strel('disk',12));
+imshow(img_bw,[])
+CC=bwconncomp(img_bw);
+stats=regionprops(CC,'Area');
+area=[stats.Area];
+AA=mean(area)+std(area);
+fusedcandidates=area>AA;
+sublist=CC.PixelIdxList(fusedcandidates);
+sublist=cat(1,sublist{:});
+fusedmask=false(size(image));
+fusedmask(sublist)=1;
+s=round(1.2*sqrt(mean(area))/pi);
+nucmin=imerode(fusedmask,strel('disk',s));
+outside=~imdilate(fusedmask,strel('disk',5));
+basin=imcomplement(bwdist(outside));
+basin=imimposemin(basin,nucmin|outside);
+pcolor(basin);shading flat;
+L=watershed(basin);
+imshow(L,[]);colormap('jet');caxis([0 20]);
+BB=img_bw-fusedmask;
+newmask=L>1|BB;
+imshow(newmask,'InitialMagnification','fit');
+imwrite(newmask,'D:\matlab_P3_4.tif','tif'); 
 end
